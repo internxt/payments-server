66345638ee45490a592f722a667f498478a60665
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-05-31 10:38:30 +0200

    refactor(users): extract db calls from service

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,36 +1,96 @@
-import { Collection, ObjectId, type MongoClient } from 'mongodb';
+import Stripe from 'stripe';
+import { FREE_PLAN_BYTES_SPACE } from '../constants';
+import { UsersRepository } from '../core/users/UsersRepository';
+import { PaymentService } from './PaymentService';
+import { StorageService } from './StorageService';
 
 export type User = {
   customerId: string;
   uuid: string;
 };
 
-type MongoUser = {
-  customer_id: string;
-  uuid: string;
-  _id: ObjectId;
-};
+export enum Product {
+  Teams = 'teams',
+  Individual = 'individual'
+}
 
 export class UsersService {
-  private readonly users: Collection<MongoUser>;
+  // private readonly users: Collection<MongoUser>;
+  private readonly usersRepository: UsersRepository;
+  private readonly paymentService: PaymentService;
+  private readonly storageService: StorageService;
 
-  constructor(mongo: MongoClient) {
-    this.users = mongo.db().collection<MongoUser>('users');
+  constructor(
+    // mongo: MongoClient, 
+    usersRepository: UsersRepository,
+    paymentsService: PaymentService,
+    storageService: StorageService
+  ) {
+    // this.users = mongo.db().collection<MongoUser>('users');
+    this.usersRepository = usersRepository;
+    this.paymentService = paymentsService;
+    this.storageService = storageService;
   }
 
   async findUserByCustomerID(customerId: User['customerId']): Promise<User> {
-    const userFound = await this.users.findOne({ customer_id: customerId });
+    const userFound = await this.usersRepository.findUserByCustomerId(customerId);
+    
     if (!userFound) {
       throw new UserNotFoundError();
     }
 
-    return this.mongoUserToUser(userFound);
+    return userFound;
   }
 
-  private mongoUserToUser(mongoUser: MongoUser): User {
-    const { customer_id: customerId, uuid } = mongoUser;
+  async findUserByUuid(uuid: User['uuid']): Promise<User> {
+    const userFound = await this.usersRepository.findUserByUuid(uuid);
+    if (!userFound) {
+      throw new UserNotFoundError();
+    }
 
-    return { customerId, uuid };
+    return userFound;
+  }
+
+  async cancelUserTeamsSubscriptions(uuid: User['uuid'], teamsUserUuid: User['uuid']) {
+    const user = await this.findUserByUuid(uuid);
+    const activeSubscriptions = await this.paymentService.getActiveSubscriptions(user.customerId);
+
+    if (activeSubscriptions.length === 0) {
+      throw new Error('Subscriptions not found');
+    }
+
+    const subscriptionsToCancel = activeSubscriptions.filter((subscription) => {
+      const isTeams = parseInt(subscription.items.data[0].price.metadata.is_teams);
+
+      return isTeams === 1;  
+    }) as Stripe.Subscription[];
+
+    for (const subscriptionToCancel of subscriptionsToCancel) {
+      await this.paymentService.cancelSubscription(subscriptionToCancel.id);
+    }
+
+    await this.storageService.changeStorage(teamsUserUuid, FREE_PLAN_BYTES_SPACE);
+  }
+
+  async cancelUserIndividualSubscriptions(uuid: User['uuid']): Promise<void> {
+    const user = await this.findUserByUuid(uuid);
+    const activeSubscriptions = await this.paymentService.getActiveSubscriptions(user.customerId);
+
+    if (activeSubscriptions.length === 0) {
+      throw new Error('Subscriptions not found');
+    }
+
+    const individualSubscriptions = activeSubscriptions.filter((subscription) => {
+      const isTeams = parseInt(subscription.items.data[0].price.metadata.is_teams);
+
+      return isTeams === 0;
+    }) as Stripe.Subscription[];
+
+    for (const subscriptionToCancel of individualSubscriptions) {
+      await this.paymentService.cancelSubscription(subscriptionToCancel.id);
+    }
+
+    await this.storageService.changeStorage(uuid, FREE_PLAN_BYTES_SPACE);
   }
 }
 
