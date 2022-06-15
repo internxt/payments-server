e1b688513b422b06f51e0acb976b0f4079acec9c
Alex Menor <alex.menor@icloud.com>
2022-06-15 18:04:09 +0200

    remove sensitive keys from logs

--- src/webhooks/index.ts
+++ src/webhooks/index.ts
@@ -31,11 +31,7 @@ export default function (
         event = stripe.webhooks.constructEvent(req.body, sig, config.STRIPE_WEBHOOK_KEY);
       } catch (err) {
         if (err instanceof Stripe.errors.StripeSignatureVerificationError) {
-          fastify.log.info(
-            `Stripe event could not be verified, sig: ${sig}, webhook_key: ${
-              config.STRIPE_WEBHOOK_KEY
-            } event: ${JSON.stringify(req.body.toString(), null, 2)}`,
-          );
+          fastify.log.info('Stripe event could not be verified');
           return rep.status(401).send();
         } else {
           throw err;
