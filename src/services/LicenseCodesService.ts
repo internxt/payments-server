4d8242b253870c945181a1c179c8c685b3705044
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-06-05 12:02:03 +0200

    refactor(code): redeem code with all the steps to emulate a payment

--- src/services/LicenseCodesService.ts
+++ src/services/LicenseCodesService.ts
@@ -2,6 +2,7 @@ import { LicenseCode } from '../core/users/LicenseCode';
 import { LicenseCodesRepository } from '../core/users/LicenseCodeRepository';
 import { User } from '../core/users/User';
 import { PaymentService } from './PaymentService';
+import { StorageService } from './StorageService';
 import { UsersService } from './UsersService';
 
 export class InvalidLicenseCodeError extends Error {
@@ -24,12 +25,16 @@ export class LicenseCodesService {
   constructor(
     private readonly paymentService: PaymentService,
     private readonly usersService: UsersService,
+    private readonly storageService: StorageService,
     private readonly licenseCodesRepository: LicenseCodesRepository
   ) {}
 
   async redeem(
-    email: string,
-    uuid: User['uuid'],
+    user: {
+      email: string, 
+      uuid: User['uuid'],
+      name?: string,
+    },
     code: LicenseCode['code'],
     provider: LicenseCode['provider'],
   ): Promise<void> {
@@ -46,16 +51,41 @@ export class LicenseCodesService {
       throw new LicenseCodeAlreadyAppliedError();
     }
 
-    const customer = await this.paymentService.createCustomer({ email });
-    await this.paymentService.subscribe(
-      customer.id,
+    const maybeExistingUser = await this.usersService.findUserByUuid(user.uuid).catch(() => null);
+    let customerId: string;
+
+    // 1. Create or get customer from Stripe
+    if (!maybeExistingUser) {
+      customerId = (await this.paymentService.createCustomer({ 
+        name: user.name || 'Internxt User',
+        email: user.email,
+      })).id;
+    } else {
+      customerId = (await this.paymentService.getCustomer(maybeExistingUser.customerId)).id;
+    }
+    
+    // 2. Subscribe to the price referenced by the code
+    const productMetadata = await this.paymentService.subscribe(
+      customerId,
       licenseCode.priceId,
-    );
-    await this.usersService.insertUser({
-      customerId: customer.id,
-      uuid,
-      lifetime: false,
-    });
+    ); 
+
+    // 3. Set the storage referenced by the code
+    await this.storageService.changeStorage(user.uuid, productMetadata.maxSpaceBytes);  
+ 
+    // 4. Update user accordingly
+    if (!maybeExistingUser) {
+      await this.usersService.insertUser({
+        customerId,
+        uuid: user.uuid,
+        lifetime: !productMetadata.recurring,
+      });
+    } else {
+      await this.usersService.updateUser(maybeExistingUser.customerId, { lifetime: !productMetadata.recurring });
+    }
+
+    // 5. Mark code as redeemed
+    await this.licenseCodesRepository.updateByCode(licenseCode.code, { redeemed: true });
   }
 
   async insertLicenseCode(licenseCode: LicenseCode): Promise<void> {
