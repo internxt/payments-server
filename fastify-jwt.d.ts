6bbf44c95765a978533cf55f59e8310c12e69d07
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2023-06-05 12:00:41 +0200

    refactor(types): add missing types on jwt

--- fastify-jwt.d.ts
+++ fastify-jwt.d.ts
@@ -4,7 +4,12 @@ declare module '@fastify/jwt' {
   interface FastifyJWT {
     user: {
       payload: {
+        email: string;
         uuid: string;
+        name: string;
+        lastname: string;
+        username: string;
+        sharedWorkspace: boolean,
         networkCredentials: {
           user: string;
           pass: string;
