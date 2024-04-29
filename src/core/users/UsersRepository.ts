61c8c2b067b840c835ad319738289e43cbe32b7b
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2024-04-29 13:51:20 +0200

    feat(users): add coupons repositories

--- src/core/users/UsersRepository.ts
+++ src/core/users/UsersRepository.ts
@@ -3,6 +3,6 @@ import { User } from './User';
 export interface UsersRepository {
   findUserByCustomerId(customerId: User['customerId']): Promise<User | null>;
   findUserByUuid(uuid: User['uuid']): Promise<User | null>;
-  insertUser(user: User): Promise<void>;
+  insertUser(user: Omit<User, 'id'>): Promise<void>;
   updateUser(customerId: User['customerId'], body: Pick<User, 'lifetime'>): Promise<boolean>;
 }
