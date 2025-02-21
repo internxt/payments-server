f09ffec47ac26e235057842286c1c5a981f2c91f
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-02-21 15:32:06 +0100

    tests: handle cancel plan tests

--- src/webhooks/utils/handleCancelPlan.ts
+++ src/webhooks/utils/handleCancelPlan.ts
@@ -1,5 +1,3 @@
-import { FastifyBaseLogger } from 'fastify';
-import CacheService from '../../services/cache.service';
 import { CustomerId } from '../../services/payment.service';
 import { UsersService } from '../../services/users.service';
 import { TiersService } from '../../services/tiers.service';
@@ -10,9 +8,7 @@ interface HandleCancelPlanProps {
   customerEmail: string;
   productId: Stripe.Product['id'];
   usersService: UsersService;
-  cacheService: CacheService;
   tiersService: TiersService;
-  log: FastifyBaseLogger;
 }
 
 export const handleCancelPlan = async ({
@@ -20,27 +16,13 @@ export const handleCancelPlan = async ({
   customerEmail,
   productId,
   usersService,
-  cacheService,
   tiersService,
-  log,
 }: HandleCancelPlanProps) => {
   const user = await usersService.findUserByCustomerID(customerId);
-  const { id: userId, lifetime: hasBoughtALifetime } = user;
+  const { id: userId } = user;
   const tier = await tiersService.getTierProductsByProductsId(productId);
 
-  if (hasBoughtALifetime) {
-    // This user has switched from a subscription to a lifetime, therefore we do not want to downgrade his space
-    // The space should not be set to Free plan.
-    return;
-  }
-
   await usersService.updateUser(customerId, { lifetime: false });
-
-  try {
-    await cacheService.clearSubscription(customerId);
-  } catch (err) {
-    log.error(`Error in handleLifetimeRefunded after trying to clear ${customerId} subscription`);
-  }
 
   await tiersService.removeTier({ ...user, email: customerEmail }, productId);
   await tiersService.deleteTierFromUser(userId, tier.id);
