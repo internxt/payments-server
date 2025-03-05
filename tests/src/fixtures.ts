ecde2ba8b809f5fb8575948956deba02a3848ddd
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-03-05 11:36:21 +0100

    tests: pyament intent endpoint

--- tests/src/fixtures.ts
+++ tests/src/fixtures.ts
@@ -107,6 +107,37 @@ export const getPrices = () => {
   };
 };
 
+export const getPrice = (params?: Stripe.Price): Stripe.Price => {
+  return {
+    id: `price_${randomDataGenerator.string({ length: 12 })}`,
+    object: 'price',
+    active: true,
+    billing_scheme: 'per_unit',
+    created: 1679431181,
+    currency: 'usd',
+    custom_unit_amount: null,
+    livemode: false,
+    lookup_key: null,
+    metadata: {},
+    nickname: null,
+    product: 'prod_NZKdYqrwEYx6iK',
+    recurring: {
+      aggregate_usage: null,
+      interval: 'month',
+      interval_count: 1,
+      trial_period_days: null,
+      usage_type: 'licensed',
+    },
+    tax_behavior: 'unspecified',
+    tiers_mode: null,
+    transform_quantity: null,
+    type: 'recurring',
+    unit_amount: 1000,
+    unit_amount_decimal: '1000',
+    ...params,
+  };
+};
+
 export const getCreateSubscriptionResponse = (params?: Partial<SubscriptionCreated>): SubscriptionCreated => {
   return {
     type: 'payment',
