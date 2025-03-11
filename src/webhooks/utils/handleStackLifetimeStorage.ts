51fd0f504656ebbd77a5b2bde47bccc2ebd7f6fc
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-03-11 16:31:28 +0100

    feat: only increment storage in handleStackLifetime

--- src/webhooks/utils/handleStackLifetimeStorage.ts
+++ src/webhooks/utils/handleStackLifetimeStorage.ts
@@ -1,10 +1,8 @@
 import { FastifyBaseLogger } from 'fastify';
-import { Service, Tier } from '../../core/users/Tier';
+import { Tier } from '../../core/users/Tier';
 import { User } from '../../core/users/User';
 import { createOrUpdateUser, updateUserTier } from '../../services/storage.service';
-import { TiersService } from '../../services/tiers.service';
 import config from '../../config';
-import Stripe from 'stripe';
 import { fetchUserStorage } from '../../utils/fetchUserStorage';
 
 export class ExpandStorageNotAvailableError extends Error {
@@ -20,9 +18,6 @@ interface HandleStackLifetimeStorageProps {
   user: User & { email: string };
   newTier: Tier;
   oldTier: Tier;
-  tiersService: TiersService;
-  customer: Stripe.Customer;
-  subscriptionSeats: Stripe.InvoiceLineItem['quantity'];
 }
 
 export const handleStackLifetimeStorage = async ({
@@ -30,12 +25,7 @@ export const handleStackLifetimeStorage = async ({
   user,
   newTier,
   oldTier,
-  tiersService,
-  subscriptionSeats,
-  customer,
 }: HandleStackLifetimeStorageProps) => {
-  const newTierId = newTier.id;
-  const oldTierId = oldTier.id;
   const newTierSpaceInBytes = newTier.featuresPerService['drive'].maxSpaceBytes;
   const oldTierSpaceInBytes = oldTier.featuresPerService['drive'].maxSpaceBytes;
 
@@ -49,23 +39,9 @@ export const handleStackLifetimeStorage = async ({
     `The user with uuid ${user.uuid} can stack more storage. Actual Storage for the user: ${userStorage.currentMaxSpaceBytes} / Storage to increase: ${newTierSpaceInBytes}`,
   );
   const totalSpaceBytes = userStorage.currentMaxSpaceBytes + newTierSpaceInBytes;
-  const tierToUpdate = newTierSpaceInBytes > oldTierSpaceInBytes ? newTier.productId : oldTier.productId;
+  const tierToUpdate = newTierSpaceInBytes >= oldTierSpaceInBytes ? newTier.productId : oldTier.productId;
 
   await createOrUpdateUser(totalSpaceBytes.toString(), user.email, config);
   await updateUserTier(user.uuid, tierToUpdate, config);
   logger.info(`The user storage has been stacked. User uuid: ${user.uuid} / Total space bytes: ${totalSpaceBytes}`);
-
-  if (newTierSpaceInBytes > oldTierSpaceInBytes) {
-    logger.info(
-      `Tier updated while stacking lifetime storage because the new one is highest than the old one. User Uuid: ${user.uuid} / tier Id: ${newTierId}`,
-    );
-    await tiersService.applyTier(user, customer, subscriptionSeats, newTier.productId, [Service.Drive]);
-
-    if (oldTierId !== newTierId) {
-      await tiersService.updateTierToUser(user.id, oldTierId, newTierId);
-      logger.info(
-        `Tier-User relationship updated while stacking lifetime storage. User uuid: ${user.uuid} / User Id: ${user.id} / new tier Id: ${newTierId}`,
-      );
-    }
-  }
 };
