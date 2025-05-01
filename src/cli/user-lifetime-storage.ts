0d46a50334bed3c0f0d320007365acb4d1c86290
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-05-01 20:29:22 +0200

    fix(cli): handling errors correctly

--- src/cli/user-lifetime-storage.ts
+++ src/cli/user-lifetime-storage.ts
@@ -6,10 +6,9 @@ import { PaymentService } from '../services/payment.service';
 import { ProductsRepository } from '../core/users/ProductsRepository';
 import { MongoDBProductsRepository } from '../core/users/MongoDBProductsRepository';
 import { MongoClient } from 'mongodb';
-import axios from 'axios';
 import { Bit2MeService } from '../services/bit2me.service';
 import { UsersRepository } from '../core/users/UsersRepository';
-import { getUserStorage, StorageService } from '../services/storage.service';
+import { getUserStorage, StorageService, UserStorage } from '../services/storage.service';
 import {
   DisplayBillingRepository,
   MongoDBDisplayBillingRepository,
@@ -22,75 +21,96 @@ import { MongoDBUsersCouponsRepository } from '../core/coupons/MongoDBUsersCoupo
 import { UserNotFoundError, UsersService } from '../services/users.service';
 import { MongoDBTiersRepository, TiersRepository } from '../core/users/MongoDBTiersRepository';
 import { MongoDBUsersTiersRepository, UsersTiersRepository } from '../core/users/MongoDBUsersTiersRepository';
-import { Tier } from '../core/users/Tier';
+import { Service, Tier } from '../core/users/Tier';
 import CacheService from '../services/cache.service';
+import axios from 'axios';
 
-async function userLifetimeStorage(startDate: number, endDate: number) {
+async function initializeStates() {
   const mongoClient = await new MongoClient(envVariablesConfig.MONGO_URI).connect();
-  try {
-    const stripe = new Stripe(envVariablesConfig.STRIPE_SECRET_KEY, { apiVersion: '2024-04-10' });
-    const usersRepository: UsersRepository = new MongoDBUsersRepository(mongoClient);
-    const storageService = new StorageService(envVariablesConfig, axios);
-    const tiersRepository: TiersRepository = new MongoDBTiersRepository(mongoClient);
-    const usersTierRepository: UsersTiersRepository = new MongoDBUsersTiersRepository(mongoClient);
-    const displayBillingRepository: DisplayBillingRepository = new MongoDBDisplayBillingRepository(mongoClient);
-    const couponsRepository: CouponsRepository = new MongoDBCouponsRepository(mongoClient);
-    const usersCouponsRepository: UsersCouponsRepository = new MongoDBUsersCouponsRepository(mongoClient);
-    const productsRepository: ProductsRepository = new MongoDBProductsRepository(mongoClient);
-    const bit2MeService: Bit2MeService = new Bit2MeService(envVariablesConfig, axios);
-    const cacheService: CacheService = new CacheService(envVariablesConfig);
+  const stripe = new Stripe(envVariablesConfig.STRIPE_SECRET_KEY, { apiVersion: '2024-04-10' });
+  const usersRepository: UsersRepository = new MongoDBUsersRepository(mongoClient);
+  const storageService = new StorageService(envVariablesConfig, axios);
+  const tiersRepository: TiersRepository = new MongoDBTiersRepository(mongoClient);
+  const usersTierRepository: UsersTiersRepository = new MongoDBUsersTiersRepository(mongoClient);
+  const displayBillingRepository: DisplayBillingRepository = new MongoDBDisplayBillingRepository(mongoClient);
+  const couponsRepository: CouponsRepository = new MongoDBCouponsRepository(mongoClient);
+  const usersCouponsRepository: UsersCouponsRepository = new MongoDBUsersCouponsRepository(mongoClient);
+  const productsRepository: ProductsRepository = new MongoDBProductsRepository(mongoClient);
+  const bit2MeService: Bit2MeService = new Bit2MeService(envVariablesConfig, axios);
+  const cacheService: CacheService = new CacheService(envVariablesConfig);
 
-    const paymentService = new PaymentService(stripe, productsRepository, bit2MeService);
-    const usersService = new UsersService(
-      usersRepository,
-      paymentService,
-      displayBillingRepository,
-      couponsRepository,
-      usersCouponsRepository,
-      envVariablesConfig,
-      axios,
-    );
-    const tiersService = new TiersService(
-      usersService,
-      paymentService,
-      tiersRepository,
-      usersTierRepository,
-      storageService,
-      envVariablesConfig,
-    );
-    const determineLifetimeConditions = new DetermineLifetimeConditions(paymentService, tiersService);
-    const report: {
-      'Customer ID': string;
-      'User UUID': string;
-      Tier: Tier['productId'];
-      'Max Space (Bytes)': number;
-      'User Storage': number;
-    }[] = [];
+  const paymentService = new PaymentService(stripe, productsRepository, bit2MeService);
+  const usersService = new UsersService(
+    usersRepository,
+    paymentService,
+    displayBillingRepository,
+    couponsRepository,
+    usersCouponsRepository,
+    envVariablesConfig,
+    axios,
+  );
+  const tiersService = new TiersService(
+    usersService,
+    paymentService,
+    tiersRepository,
+    usersTierRepository,
+    storageService,
+    envVariablesConfig,
+  );
+  const determineLifetimeConditions = new DetermineLifetimeConditions(paymentService, tiersService);
 
-    const invoices = await stripe.invoices
-      .list({
-        created: {
-          gte: startDate,
-          lte: endDate,
-        },
-        status: 'paid',
-        limit: 100,
-      })
-      .autoPagingToArray({ limit: 10000 });
+  return {
+    mongoClient,
+    cacheService,
+    paymentService,
+    tiersService,
+    determineLifetimeConditions,
+    usersService,
+    storageService,
+    stripe,
+  };
+}
 
-    const filteredInvoices = invoices.filter((invoice) => {
-      const hasValidProduct = invoice.lines.data.some((line) => {
-        const product = line.price?.product;
-        return typeof product === 'string' && line.price?.metadata.planType === 'one_time';
-      });
+const report: {
+  'Customer ID': string;
+  'User UUID': string;
+  Tier: Tier['productId'];
+  'Max Space (Bytes)': number;
+  'User Storage': number;
+}[] = [];
 
-      return invoice.customer && hasValidProduct && invoice.charge;
+const filteredInvoices = async (stripe: Stripe, startDate: number, endDate: number) => {
+  const invoices = await stripe.invoices
+    .list({
+      created: {
+        gte: startDate,
+        lte: endDate,
+      },
+      status: 'paid',
+      limit: 100,
+    })
+    .autoPagingToArray({ limit: 10000 });
+
+  return invoices.filter((invoice) => {
+    const hasValidProduct = invoice.lines.data.some((line) => {
+      const product = line.price?.product;
+      return typeof product === 'string' && line.price?.metadata.planType === 'one_time';
     });
 
-    const seenUsers = new Set<string>();
+    return invoice.customer && hasValidProduct && invoice.charge;
+  });
+};
+
+async function userLifetimeStorage(startDate: number, endDate: number) {
+  const seenUsers = new Set<string>();
+  const { cacheService, determineLifetimeConditions, mongoClient, storageService, usersService, stripe, tiersService } =
+    await initializeStates();
+  try {
+    const invoices = await filteredInvoices(stripe, startDate, endDate);
+
     console.time('table-process');
 
-    for (const invoice of filteredInvoices) {
+    for (const invoice of invoices) {
       const user = await usersService.findUserByCustomerID(invoice.customer as string).catch((err) => {
         if (!(err instanceof UserNotFoundError)) {
           throw err;
@@ -101,11 +121,11 @@ async function userLifetimeStorage(startDate: number, endDate: number) {
 
       if (!user) {
         console.warn(
-          `⚠️ User with customer Id ${invoice.customer as string} has not found in our local DB. Skipping this user...`,
+          `[${invoice.id}] ⚠️ User with customer Id ${invoice.customer as string} has not found in our local DB. Skipping this user...`,
         );
         continue;
       } else if (!user?.lifetime) {
-        console.warn(`⚠️ The user ${user.uuid} does not have a lifetime. Skipping this user...`);
+        console.warn(`[${invoice.id}] ⚠️ The user ${user.uuid} does not have a lifetime. Skipping this user...`);
         continue;
       }
 
@@ -124,81 +144,115 @@ async function userLifetimeStorage(startDate: number, endDate: number) {
       });
 
       if (!tierExists) {
-        console.info(`Product with id ${productId} is not a new product (does not have a tier). Skipping it...`);
+        console.info(
+          `[${invoice.id}] Product with id ${productId} is not a new product (does not have a tier). Skipping it...`,
+        );
         continue;
       }
 
       const customer = await stripe.customers.retrieve(user.customerId);
       if (customer.deleted) return;
-      const userCharges = await paymentService.retrieveCustomerCharges(customer.id);
 
-      userCharges.data.map((charge) => {
-        if (!charge.invoice) {
-          console.log(`Charge ${charge.id} for user ${customer.id} does not have an invoice`);
-        }
-      });
-
-      let userStorage: {
-        canExpand: boolean;
-        currentMaxSpaceBytes: number;
-        expandableBytes: number;
-      };
+      let userStorage: UserStorage;
 
       try {
         userStorage = await getUserStorage(user.uuid, customer.email as string, '0', envVariablesConfig);
       } catch {
-        console.error(`The user with UUID: ${user.uuid} does not exist in Drive Server WIP. Skipping this user...`);
+        console.error(
+          `[${invoice.id}] The user with UUID: ${user.uuid} does not exist in Drive Server WIP. Skipping this user...`,
+        );
         continue;
       }
 
-      const { maxSpaceBytes, tier } = await determineLifetimeConditions.determine(user, productId);
+      let userMaxSpaceBytes: number;
+      let tier: Tier;
 
-      if (maxSpaceBytes !== userStorage.currentMaxSpaceBytes) {
-        // try {
-        //   const userActualTiers = await tiersService.getTiersProductsByUserId(user.id).catch((err) => {
-        //     if (!(err instanceof TierNotFoundError)) {
-        //       throw err;
-        //     }
-        //     return null;
-        //   });
+      try {
+        const userTier = await determineLifetimeConditions.determine(user, productId);
+        userMaxSpaceBytes = userTier.maxSpaceBytes;
+        tier = userTier.tier;
+      } catch {
+        console.error(
+          `[${invoice.id}] Something went wrong while determining the user lifetime conditions. Customer ID: ${customer.id}`,
+        );
+        continue;
+      }
 
-        //   if (!tier) {
-        //     throw new Error(`Tier not defined for user ${user.uuid}`);
-        //   }
+      if (!userMaxSpaceBytes && !tier) {
+        continue;
+      }
 
-        //   const individualUserTier = userActualTiers?.find(
-        //     (tier) => !tier.featuresPerService[Service.Drive].workspaces.enabled,
-        //   );
+      if (userMaxSpaceBytes !== userStorage.currentMaxSpaceBytes) {
+        let userTiers: Tier[] | null;
 
-        //   if (!individualUserTier) {
-        //     await tiersService.insertTierToUser(user.id, tier.id);
-        //   } else if (individualUserTier.id !== tier.id) {
-        //     await tiersService.updateTierToUser(user.id, individualUserTier.id, tier.id);
-        //   }
+        userTiers = await tiersService.getTiersProductsByUserId(user.id).catch((err) => {
+          if (!(err instanceof TierNotFoundError)) {
+            console.error(`Something went wrong fetching the user tiers: ${err.message}`);
+            throw err;
+          }
 
-        //   await storageService.changeStorage(user.uuid, maxSpaceBytes);
+          console.log(`[${invoice.id}] The user with customer ID ${user.customerId} does not have any active tier`);
+          return null;
+        });
 
-        //   await tiersService.applyTier(
-        //     {
-        //       ...user,
-        //       email: customer.email as string,
-        //     },
-        //     customer,
-        //     1,
-        //     productId,
-        //     [Service.Drive],
-        //   );
+        const individualUserTier = userTiers?.find(
+          (tier) => !tier.featuresPerService[Service.Drive].workspaces.enabled,
+        );
 
-        //   await cacheService.clearSubscription(user.customerId);
-        // } catch (error) {
-        //   console.error(`Error managing lifetime tier for user ${user.id}:`, error);
-        //   continue;
-        // }
+        try {
+          if (!individualUserTier) {
+            await tiersService.insertTierToUser(user.id, tier.id);
+          } else if (individualUserTier.id !== tier.id) {
+            await tiersService.updateTierToUser(user.id, individualUserTier.id, tier.id);
+          }
+        } catch (error) {
+          console.error(
+            `[${invoice.id}] Error while inserting or updating the user-tier relationship. CUSTOMER ID: ${user.customerId} - PRODUCT ID (TIER): ${tier.productId}. ERROR: ${error.stack ?? error.message}`,
+          );
+          continue;
+        }
+
+        try {
+          await storageService.changeStorage(user.uuid, userMaxSpaceBytes);
+        } catch (error) {
+          console.error(
+            `[${invoice.id}] Error while updating the user storage. CUSTOMER ID: ${user.customerId}. ERROR: ${error.stack ?? error.message}`,
+          );
+          continue;
+        }
+
+        try {
+          await tiersService.applyTier(
+            {
+              ...user,
+              email: customer.email as string,
+            },
+            customer,
+            1,
+            productId,
+            [Service.Drive],
+          );
+        } catch (error) {
+          console.error(
+            `[${invoice.id}] Error applying lifetime tier for user with customer ID ${user.customerId}:`,
+            error,
+          );
+          continue;
+        }
+
+        try {
+          await cacheService.clearSubscription(user.customerId);
+        } catch {
+          console.error(
+            `[${invoice.id}] Error while clearing subscription cache for user with customer ID: ${user.customerId}`,
+          );
+        }
+
         report.push({
           'Customer ID': customer.id,
           'User UUID': user.uuid,
           Tier: tier.productId,
-          'Max Space (Bytes)': maxSpaceBytes,
+          'Max Space (Bytes)': userMaxSpaceBytes,
           'User Storage': userStorage.currentMaxSpaceBytes,
         });
       }
