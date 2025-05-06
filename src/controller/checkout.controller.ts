849c60963642188dff18e5354361a842440a30f3
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-05-06 12:09:46 +0200

    feat(checkout): get customer Id endpoint

---
+++ src/controller/checkout.controller.ts
@@ -0,0 +1,68 @@
+import fastifyJwt from '@fastify/jwt';
+
+import { FastifyInstance } from 'fastify';
+import config from '../config';
+import fastifyRateLimit from '@fastify/rate-limit';
+import { UsersService } from '../services/users.service';
+import { PaymentService } from '../services/payment.service';
+import jwt from 'jsonwebtoken';
+
+export default function (usersService: UsersService, paymentsService: PaymentService) {
+  return async function (fastify: FastifyInstance) {
+    fastify.register(fastifyJwt, { secret: config.JWT_SECRET });
+    fastify.register(fastifyRateLimit, {
+      max: 1000,
+      timeWindow: '1 minute',
+    });
+
+    fastify.addHook('onRequest', async (request, reply) => {
+      try {
+        await request.jwtVerify();
+      } catch (err) {
+        request.log.warn(`JWT verification failed with error: ${(err as Error).message}`);
+        reply.status(401).send();
+      }
+    });
+
+    function signUserToken(customerId: string) {
+      return jwt.sign({ customerId }, config.JWT_SECRET);
+    }
+
+    fastify.get<{ Params: { country: string; companyVatId: string } }>(
+      '/customer',
+      {
+        schema: {
+          params: {
+            country: { type: 'string' },
+            companyVatId: { type: 'string' },
+          },
+        },
+        config: {
+          rateLimit: {
+            max: 5,
+            timeWindow: '1 hour',
+          },
+        },
+      },
+      async (req, res): Promise<{ customerId: string; token: string }> => {
+        const { country, companyVatId } = req.params;
+        const { uuid: userUuid, email, name } = req.user.payload;
+
+        const userExists = await usersService.findUserByUuid(userUuid).catch(() => null);
+
+        if (userExists) {
+          const { customerId } = userExists;
+          return res.status(200).send({ customerId: customerId, token: signUserToken(customerId) });
+        }
+
+        const { id: customerId } = await paymentsService.createCustomer({ name, email });
+
+        if (country && companyVatId) {
+          await paymentsService.getVatIdAndAttachTaxIdToCustomer(customerId, country, companyVatId);
+        }
+
+        return res.send({ customerId, token: signUserToken(customerId) });
+      },
+    );
+  };
+}
