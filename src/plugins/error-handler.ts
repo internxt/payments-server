0e2630466db03101f280075f2a6b018cbac43dfa
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-05-06 16:07:43 +0200

    fix(errors): remove constructor name from response

--- src/plugins/error-handler.ts
+++ src/plugins/error-handler.ts
@@ -1,19 +1,16 @@
 import { FastifyInstance, FastifyReply } from 'fastify';
 import { HttpError } from '../errors/HttpError';
-import { InternalServerError } from '../errors/Errors';
 
 export function registerErrorHandler(app: FastifyInstance) {
   app.setErrorHandler((error, _, reply: FastifyReply) => {
     if (error instanceof HttpError) {
       return reply.status(error.statusCode).send({
-        error: error.name,
         message: error.message,
       });
     }
 
-    return reply.status(500).send({
-      error: InternalServerError.name,
-      message: 'Something went wrong',
+    return reply.status(error.statusCode ?? 500).send({
+      message: error.message,
     });
   });
 }
--- tests/src/plugins/error-handler.test.ts
+++ tests/src/plugins/error-handler.test.ts
@@ -1,7 +1,6 @@
 // tests/error-handler.test.ts
 import fastify, { FastifyInstance } from 'fastify';
 import { testRoutes } from '../utils/error-test-routes';
-import { BadRequestError, NotFoundError, InternalServerError } from '../../../src/errors/Errors';
 import { registerErrorHandler } from '../../../src/plugins/error-handler';
 
 let app: FastifyInstance;
@@ -26,7 +25,6 @@ describe('Custom error handler', () => {
 
     expect(res.statusCode).toBe(400);
     expect(JSON.parse(res.body)).toEqual({
-      error: BadRequestError.name,
       message: 'Missing parameter',
     });
   });
@@ -39,7 +37,6 @@ describe('Custom error handler', () => {
 
     expect(res.statusCode).toBe(404);
     expect(JSON.parse(res.body)).toEqual({
-      error: NotFoundError.name,
       message: 'User not found',
     });
   });
@@ -52,7 +49,6 @@ describe('Custom error handler', () => {
 
     expect(res.statusCode).toBe(500);
     expect(JSON.parse(res.body)).toEqual({
-      error: InternalServerError.name,
       message: 'Something went wrong',
     });
   });
