f2d7dfcdff5c0ce13d1f22e4b1b89a3499f0023f
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-06-04 17:51:31 +0200

    tests(webhook): coverage for invoice.payment_failed event

--- tests/src/webhooks/webhook.test.ts
+++ tests/src/webhooks/webhook.test.ts
@@ -1,14 +1,18 @@
 import { FastifyInstance } from 'fastify';
 import { closeServerAndDatabase, initializeServerAndDatabase } from '../utils/initializeServer';
 import Stripe from 'stripe';
-import { getLogger, getPaymentIntent } from '../fixtures';
+import { getInvoice, getPaymentIntent } from '../fixtures';
 import handleFundsCaptured from '../../../src/webhooks/handleFundsCaptured';
 import { PaymentService } from '../../../src/services/payment.service';
 import { ObjectStorageService } from '../../../src/services/objectStorage.service';
+import handleInvoicePaymentFailed from '../../../src/webhooks/handleInvoicePaymentFailed';
 
 let app: FastifyInstance;
 
 jest.mock('../../../src/webhooks/handleFundsCaptured');
+jest.mock('../../../src/webhooks/handleInvoicePaymentFailed');
+
+const secret = 'whsec_lorim_ipsum_etc_etc';
 
 beforeAll(async () => {
   app = await initializeServerAndDatabase();
@@ -22,7 +26,6 @@ afterAll(async () => {
 describe('Webhook events', () => {
   describe('The webhooks are called correctly', () => {
     it('When the event payment_intent.amount_capturable_updated is triggered, then the correct function is called', async () => {
-      const logger = getLogger();
       const mockedPaymentIntent = getPaymentIntent();
       const event = {
         id: 'evt_1',
@@ -30,7 +33,6 @@ describe('Webhook events', () => {
         data: { object: mockedPaymentIntent },
       };
       const payloadToString = JSON.stringify(event);
-      const secret = 'whsec_lorim_ipsum_etc_etc';
 
       const header = Stripe.webhooks.generateTestHeaderString({
         payload: payloadToString,
@@ -54,6 +56,40 @@ describe('Webhook events', () => {
         expect.any(PaymentService),
         expect.any(ObjectStorageService),
         expect.any(Stripe),
+        app.log,
+      );
+    });
+
+    it('When the event invoice.payment_failed is triggered, then the correct function is called', async () => {
+      const mockedInvoice = getInvoice();
+      const event = {
+        id: 'evt_2',
+        type: 'invoice.payment_failed',
+        data: { object: mockedInvoice },
+      };
+      const payloadToString = JSON.stringify(event);
+
+      const header = Stripe.webhooks.generateTestHeaderString({
+        payload: payloadToString,
+        secret,
+      });
+
+      const response = await app.inject({
+        method: 'POST',
+        path: 'webhook',
+        body: Buffer.from(payloadToString),
+        headers: {
+          'stripe-signature': header,
+          'content-type': 'application/json',
+        },
+      });
+
+      expect(response.statusCode).toBe(204);
+      expect(handleInvoicePaymentFailed).toHaveBeenCalled();
+      expect(handleInvoicePaymentFailed).toHaveBeenCalledWith(
+        event.data.object,
+        expect.any(ObjectStorageService),
+        expect.any(PaymentService),
         app.log,
       );
     });
