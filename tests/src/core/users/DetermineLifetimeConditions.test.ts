70b2042fe84f05eb4b865645942ce02d44e5510d
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-06-09 12:02:10 +0200

    tests: verify that the maxSpaceBytes is the sum of all spaceBytes

--- tests/src/core/users/DetermineLifetimeConditions.test.ts
+++ tests/src/core/users/DetermineLifetimeConditions.test.ts
@@ -15,7 +15,7 @@ import testFactory from '../../utils/factory';
 import config from '../../../../src/config';
 import Stripe from 'stripe';
 import { DetermineLifetimeConditions } from '../../../../src/core/users/DetermineLifetimeConditions';
-import { getUser, newTier, getCustomer, getInvoice, getSubscription } from '../../fixtures';
+import { getUser, newTier, getCustomer, getInvoice, getSubscription, getInvoices } from '../../fixtures';
 import { Service } from '../../../../src/core/users/Tier';
 import { BadRequestError, InternalServerError } from '../../../../src/errors/Errors';
 
@@ -151,16 +151,27 @@ describe('Determining Lifetime conditions', () => {
     it('When the user already has a lifetime, then the storage should be stacked', async () => {
       const mockedUser = getUser({ lifetime: true });
       const mockedTier = newTier();
-      jest.spyOn(determineLifetimeConditions as any, 'handleStackingLifetime').mockResolvedValue({
-        tier: mockedTier,
-        maxSpaceBytes: mockedTier.featuresPerService[Service.Drive].maxSpaceBytes,
-      });
+      const mockedInvoices = getInvoices();
+      mockedInvoices.forEach((invoice) => (invoice.lines.data[0].price!.metadata!.maxSpaceBytes = '1000000'));
+      const totalMaxSpaceBytes = mockedInvoices.reduce(
+        (acc, invoice) => acc + parseInt(invoice.lines.data[0].price!.metadata!.maxSpaceBytes ?? '0'),
+        0,
+      );
+
       jest.spyOn(paymentService, 'getUserSubscription').mockResolvedValue({ type: 'free' });
       jest.spyOn(tiersService, 'getTierProductsByProductsId').mockResolvedValue(mockedTier);
+      jest.spyOn(tiersService, 'getTiersProductsByUserId').mockResolvedValue([mockedTier]);
+      jest.spyOn(paymentService, 'getInvoicesFromUser').mockResolvedValue(mockedInvoices);
+      jest
+        .spyOn(paymentService, 'getCustomer')
+        .mockResolvedValue(getCustomer({ id: mockedUser.customerId }) as Stripe.Response<Stripe.Customer>);
+      jest.spyOn(paymentService, 'getCustomersByEmail').mockResolvedValue([getCustomer({ id: mockedUser.customerId })]);
+      jest.spyOn(determineConditions, 'getPaidInvoices').mockResolvedValue(mockedInvoices);
+      jest.spyOn(determineConditions, 'getHigherTier').mockResolvedValue(mockedTier);
 
       const { maxSpaceBytes, tier } = await determineLifetimeConditions.determine(mockedUser, mockedTier.productId);
 
-      expect(maxSpaceBytes).toStrictEqual(mockedTier.featuresPerService[Service.Drive].maxSpaceBytes);
+      expect(maxSpaceBytes).toStrictEqual(totalMaxSpaceBytes);
       expect(tier).toStrictEqual(mockedTier);
     });
   });
