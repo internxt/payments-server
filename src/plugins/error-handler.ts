6fde0fe4ff88a8356ca0e3f25c1eeba7c6b8ced4
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-06-12 11:30:20 +0200

    feat: handling error responses better

--- src/plugins/error-handler.ts
+++ src/plugins/error-handler.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
 import { HttpError } from '../errors/HttpError';
+import { isProduction } from '../config';
 
 export function registerErrorHandler(app: FastifyInstance) {
   app.setErrorHandler((error, request: FastifyRequest, reply: FastifyReply) => {
@@ -9,25 +10,27 @@ export function registerErrorHandler(app: FastifyInstance) {
       });
     }
 
-    const statusCode = error instanceof HttpError ? error.statusCode : (error.statusCode ?? 500);
+    const statusCode = error.statusCode ?? 500;
     const errorMessage = error.message || 'Unknown error';
 
-    request.log.error(
-      {
-        method: request.method,
-        url: request.url,
-        statusCode,
-        errorMessage,
-        stack: error.stack,
-        query: request.query,
-        body: request.body,
-        params: request.params,
-      },
-      'Unhandled error',
-    );
+    if (!isProduction) {
+      request.log.error(
+        {
+          method: request.method,
+          url: request.url,
+          statusCode,
+          errorMessage,
+          stack: error.stack,
+          query: request.query,
+          body: request.body,
+          params: request.params,
+        },
+        'Unhandled error',
+      );
+    }
 
-    return reply.status(500).send({
-      message: 'Internal server error',
+    return reply.status(statusCode ?? 500).send({
+      message: statusCode > 499 ? 'Internal Server Error' : error.message,
     });
   });
 }
