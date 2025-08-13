719bab090634ffdb18578d029da708f4fc2c1baf
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-08-13 19:13:23 +0200

    test: handleStackLifetimeStorage

--- tests/src/webhooks/utils/handleStackLifetimeStorage.test.ts
+++ tests/src/webhooks/utils/handleStackLifetimeStorage.test.ts
@@ -1,25 +1,12 @@
-import axios from 'axios';
-import Stripe from 'stripe';
 import config from '../../../../src/config';
-import { CouponsRepository } from '../../../../src/core/coupons/CouponsRepository';
-import { UsersCouponsRepository } from '../../../../src/core/coupons/UsersCouponsRepository';
-import { DisplayBillingRepository } from '../../../../src/core/users/MongoDBDisplayBillingRepository';
-import { TiersRepository } from '../../../../src/core/users/MongoDBTiersRepository';
-import { UsersTiersRepository } from '../../../../src/core/users/MongoDBUsersTiersRepository';
-import { ProductsRepository } from '../../../../src/core/users/ProductsRepository';
-import { UsersRepository } from '../../../../src/core/users/UsersRepository';
-import { Bit2MeService } from '../../../../src/services/bit2me.service';
-import { PaymentService } from '../../../../src/services/payment.service';
-import { StorageService, updateUserTier } from '../../../../src/services/storage.service';
-import { TiersService } from '../../../../src/services/tiers.service';
-import { UsersService } from '../../../../src/services/users.service';
-import testFactory from '../../utils/factory';
+import { updateUserTier } from '../../../../src/services/storage.service';
 import { getLogger, newTier as getTier, getUser, voidPromise } from '../../fixtures';
 import {
   ExpandStorageNotAvailableError,
   handleStackLifetimeStorage,
 } from '../../../../src/webhooks/utils/handleStackLifetimeStorage';
 import { fetchUserStorage } from '../../../../src/utils/fetchUserStorage';
+import { createTestServices } from '../../helpers/services-factory';
 
 jest.mock('../../../../src/utils/fetchUserStorage');
 
@@ -33,67 +20,21 @@ jest.mock('../../../../src/services/storage.service', () => {
   };
 });
 
-let usersRepository: UsersRepository;
-let displayBillingRepository: DisplayBillingRepository;
-let couponsRepository: CouponsRepository;
-let usersCouponsRepository: UsersCouponsRepository;
-let productsRepository: ProductsRepository;
-let tiersRepository: TiersRepository;
-let usersTiersRepository: UsersTiersRepository;
-let bit2MeService: Bit2MeService;
-let usersService: UsersService;
-let paymentService: PaymentService;
-let storageService: StorageService;
-let tiersService: TiersService;
+let mockedUser = { ...getUser(), email: 'example@inxt.com' };
+let mockedLogger = getLogger();
+let mockedOldTier = getTier();
+let mockedNewTier = getTier();
+
+let services: ReturnType<typeof createTestServices>;
+
+beforeEach(() => {
+  services = createTestServices();
+  jest.clearAllMocks();
+});
+
+afterEach(() => jest.restoreAllMocks());
 
 describe('Stack lifetime storage', () => {
-  let mockedUser = { ...getUser(), email: 'example@inxt.com' };
-  let mockedLogger = getLogger();
-  let mockedOldTier = getTier();
-  let mockedNewTier = getTier();
-  beforeEach(() => {
-    usersRepository = testFactory.getUsersRepositoryForTest();
-    displayBillingRepository = {} as DisplayBillingRepository;
-    couponsRepository = testFactory.getCouponsRepositoryForTest();
-    usersCouponsRepository = testFactory.getUsersCouponsRepositoryForTest();
-    storageService = new StorageService(config, axios);
-    productsRepository = testFactory.getProductsRepositoryForTest();
-    tiersRepository = testFactory.getTiersRepository();
-    usersTiersRepository = testFactory.getUsersTiersRepository();
-    bit2MeService = new Bit2MeService(config, axios);
-    paymentService = new PaymentService(
-      new Stripe(config.STRIPE_SECRET_KEY, { apiVersion: '2024-04-10' }),
-      productsRepository,
-      bit2MeService,
-    );
-
-    usersService = new UsersService(
-      usersRepository,
-      paymentService,
-      displayBillingRepository,
-      couponsRepository,
-      usersCouponsRepository,
-      config,
-      axios,
-    );
-
-    tiersService = new TiersService(
-      usersService,
-      paymentService,
-      tiersRepository,
-      usersTiersRepository,
-      storageService,
-      config,
-    );
-
-    mockedUser = { ...getUser(), email: 'example@inxt.com' };
-    mockedLogger = getLogger();
-    mockedOldTier = getTier();
-    mockedNewTier = getTier();
-  });
-
-  afterEach(() => jest.restoreAllMocks());
-
   it("When the user storage can't be expanded, then an error indicating so is thrown", async () => {
     (fetchUserStorage as jest.Mock).mockResolvedValue({
       canExpand: false,
@@ -104,7 +45,7 @@ describe('Stack lifetime storage', () => {
         logger: mockedLogger,
         newTier: mockedNewTier,
         oldTier: mockedOldTier,
-        storageService,
+        storageService: services.storageService,
         user: mockedUser,
       }),
     ).rejects.toThrow(ExpandStorageNotAvailableError);
@@ -121,14 +62,14 @@ describe('Stack lifetime storage', () => {
       canExpand: true,
       currentMaxSpaceBytes: mockedOldTier.featuresPerService['drive'].maxSpaceBytes,
     });
-    const changeStorageSpy = jest.spyOn(storageService, 'changeStorage').mockImplementation(voidPromise);
+    const changeStorageSpy = jest.spyOn(services.storageService, 'changeStorage').mockImplementation(voidPromise);
     (updateUserTier as jest.Mock).mockImplementation();
 
     await handleStackLifetimeStorage({
       logger: mockedLogger,
       newTier: mockedNewTier,
       oldTier: mockedOldTier,
-      storageService,
+      storageService: services.storageService,
       user: mockedUser,
     });
 
