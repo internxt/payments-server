151d5e833c0ffa5f8f00cd03728e4a04d910b13f
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-08-14 13:31:03 +0200

    feat: destructuring services in handleLifetimeRefunded test

--- tests/src/webhooks/handleLifetimeRefunded.test.ts
+++ tests/src/webhooks/handleLifetimeRefunded.test.ts
@@ -1,39 +1,13 @@
 import Stripe from 'stripe';
-import { CouponsRepository } from '../../../src/core/coupons/CouponsRepository';
-import { UsersCouponsRepository } from '../../../src/core/coupons/UsersCouponsRepository';
-import { DisplayBillingRepository } from '../../../src/core/users/MongoDBDisplayBillingRepository';
-import { TiersRepository } from '../../../src/core/users/MongoDBTiersRepository';
-import { UsersTiersRepository } from '../../../src/core/users/MongoDBUsersTiersRepository';
-import { ProductsRepository } from '../../../src/core/users/ProductsRepository';
-import { UsersRepository } from '../../../src/core/users/UsersRepository';
-import { Bit2MeService } from '../../../src/services/bit2me.service';
-import CacheService from '../../../src/services/cache.service';
-import { PaymentService } from '../../../src/services/payment.service';
-import { StorageService, updateUserTier } from '../../../src/services/storage.service';
-import { TierNotFoundError, TiersService } from '../../../src/services/tiers.service';
-import { UsersService } from '../../../src/services/users.service';
-import { FastifyBaseLogger } from 'fastify';
+import { updateUserTier } from '../../../src/services/storage.service';
+import { TierNotFoundError } from '../../../src/services/tiers.service';
 import { getCharge, getInvoice, getLogger, getUser } from '../fixtures';
-import testFactory from '../utils/factory';
 import config from '../../../src/config';
-import axios from 'axios';
-import { ObjectStorageService } from '../../../src/services/objectStorage.service';
 import { handleCancelPlan } from '../../../src/webhooks/utils/handleCancelPlan';
 import handleLifetimeRefunded from '../../../src/webhooks/handleLifetimeRefunded';
 import { FREE_INDIVIDUAL_TIER, FREE_PLAN_BYTES_SPACE } from '../../../src/constants';
+import { createTestServices } from '../helpers/services-factory';
 
-jest.mock('stripe', () => {
-  return {
-    __esModule: true,
-    default: jest.fn().mockImplementation(),
-  };
-});
-jest.mock('../../../src/services/cache.service', () => {
-  return {
-    __esModule: true,
-    default: jest.fn().mockImplementation(),
-  };
-});
 jest.mock('../../../src/webhooks/utils/handleCancelPlan');
 jest.mock('../../../src/services/storage.service', () => {
   const actualModule = jest.requireActual('../../../src/services/storage.service');
@@ -44,60 +18,10 @@ jest.mock('../../../src/services/storage.service', () => {
   };
 });
 
-let paymentService: PaymentService;
-let storageService: StorageService;
-let usersService: UsersService;
-let usersRepository: UsersRepository;
-let displayBillingRepository: DisplayBillingRepository;
-let couponsRepository: CouponsRepository;
-let usersCouponsRepository: UsersCouponsRepository;
-let tiersService: TiersService;
-let tiersRepository: TiersRepository;
-let usersTiersRepository: UsersTiersRepository;
-let productsRepository: ProductsRepository;
-let bit2MeService: Bit2MeService;
-let cacheService: CacheService;
-let stripe: Stripe;
-let objectStorageService: ObjectStorageService;
-let logger: jest.Mocked<FastifyBaseLogger>;
+const logger = getLogger();
+const { paymentService, usersService, storageService, tiersService, cacheService } = createTestServices();
 
 beforeEach(() => {
-  logger = getLogger();
-
-  stripe = new Stripe('mock-key', { apiVersion: '2024-04-10' }) as jest.Mocked<Stripe>;
-  usersRepository = testFactory.getUsersRepositoryForTest();
-  displayBillingRepository = {} as DisplayBillingRepository;
-  couponsRepository = testFactory.getCouponsRepositoryForTest();
-  usersCouponsRepository = testFactory.getUsersCouponsRepositoryForTest();
-  productsRepository = testFactory.getProductsRepositoryForTest();
-  tiersRepository = testFactory.getTiersRepository();
-  usersTiersRepository = testFactory.getUsersTiersRepository();
-
-  cacheService = new CacheService(config);
-  storageService = new StorageService(config, axios);
-  bit2MeService = new Bit2MeService(config, axios);
-  paymentService = new PaymentService(stripe, productsRepository, bit2MeService);
-
-  tiersService = new TiersService(
-    usersService,
-    paymentService,
-    tiersRepository,
-    usersTiersRepository,
-    storageService,
-    config,
-  );
-  usersService = new UsersService(
-    usersRepository,
-    paymentService,
-    displayBillingRepository,
-    couponsRepository,
-    usersCouponsRepository,
-    config,
-    axios,
-  );
-
-  objectStorageService = new ObjectStorageService(paymentService, config, axios);
-
   jest.clearAllMocks();
 });
 
@@ -134,8 +58,8 @@ describe('Process when a lifetime is refunded', () => {
       customerEmail: mockedCharge.receipt_email,
       productId: (mockedInvoiceLineItems.data[0].price?.product as Stripe.Product).id,
       isLifetime: mockedUser.lifetime,
-      usersService,
-      tiersService,
+      usersService: usersService,
+      tiersService: tiersService,
       log: logger,
     });
   });
