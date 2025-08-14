91ba4ffb3f33f5b32b6e9546b343f90507f96b5b
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2025-08-14 11:16:28 +0200

    feat: invoiceCompletedHandler

--- tests/src/webhooks/events/invoices/InvoiceCompletedHandler.test.ts
+++ tests/src/webhooks/events/invoices/InvoiceCompletedHandler.test.ts
@@ -1,116 +1,24 @@
 import Stripe from 'stripe';
-import axios from 'axios';
 
-import { CouponsRepository } from '../../../../../src/core/coupons/CouponsRepository';
-import { UsersCouponsRepository } from '../../../../../src/core/coupons/UsersCouponsRepository';
-import { DisplayBillingRepository } from '../../../../../src/core/users/MongoDBDisplayBillingRepository';
-import { TiersRepository } from '../../../../../src/core/users/MongoDBTiersRepository';
-import { UsersTiersRepository } from '../../../../../src/core/users/MongoDBUsersTiersRepository';
-import { ProductsRepository } from '../../../../../src/core/users/ProductsRepository';
-import { UsersRepository } from '../../../../../src/core/users/UsersRepository';
-import { Bit2MeService } from '../../../../../src/services/bit2me.service';
-import CacheService from '../../../../../src/services/cache.service';
-import { PaymentService } from '../../../../../src/services/payment.service';
-import { StorageService } from '../../../../../src/services/storage.service';
-import { CouponNotBeingTrackedError, UserNotFoundError, UsersService } from '../../../../../src/services/users.service';
-import { getCustomer, getInvoice, getLogger, getProduct, getUser, newTier, voidPromise } from '../../../fixtures';
-import { ObjectStorageService } from '../../../../../src/services/objectStorage.service';
-import {
-  InvoiceCompletedHandler,
-  InvoiceCompletedHandlerPayload,
-} from '../../../../../src/webhooks/events/invoices/InvoiceCompletedHandler';
-import { ObjectStorageWebhookHandler } from '../../../../../src/webhooks/events/ObjectStorageWebhookHandler';
-import { TierNotFoundError, TiersService, UsersTiersError } from '../../../../../src/services/tiers.service';
-import config from '../../../../../src/config';
-import testFactory from '../../../utils/factory';
-import { DetermineLifetimeConditions } from '../../../../../src/core/users/DetermineLifetimeConditions';
+import { CouponNotBeingTrackedError, UserNotFoundError } from '../../../../../src/services/users.service';
+import { getCustomer, getInvoice, getProduct, getUser, newTier, voidPromise } from '../../../fixtures';
+import { InvoiceCompletedHandlerPayload } from '../../../../../src/webhooks/events/invoices/InvoiceCompletedHandler';
+import { TierNotFoundError, UsersTiersError } from '../../../../../src/services/tiers.service';
 import { NotFoundError } from '../../../../../src/errors/Errors';
 import Logger from '../../../../../src/Logger';
 import { Service } from '../../../../../src/core/users/Tier';
+import { createTestServices } from '../../../helpers/services-factory';
 
-jest.mock('ioredis', () => {
-  const mockRedis = {
-    get: jest.fn(),
-    set: jest.fn(),
-    del: jest.fn(),
-    disconnect: jest.fn(),
-    quit: jest.fn(),
-  };
-  return jest.fn(() => mockRedis);
+const { invoiceCompletedHandler, paymentService, tiersService, usersService } = createTestServices();
+
+beforeEach(() => {
+  jest.clearAllMocks();
+  jest.restoreAllMocks();
 });
 
-let paymentService: PaymentService;
-let storageService: StorageService;
-let usersService: UsersService;
-let usersRepository: UsersRepository;
-let displayBillingRepository: DisplayBillingRepository;
-let tierRepository: TiersRepository;
-let usersTiersRepository: UsersTiersRepository;
-let couponsRepository: CouponsRepository;
-let usersCouponsRepository: UsersCouponsRepository;
-let productsRepository: ProductsRepository;
-let bit2MeService: Bit2MeService;
-let cacheService: CacheService;
-let stripe: Stripe;
-let objectStorageService: ObjectStorageService;
-let invoiceCompletedHandler: InvoiceCompletedHandler;
-let determineLifetimeConditions: DetermineLifetimeConditions;
-let objectStorageWebhookHandler: ObjectStorageWebhookHandler;
-let tiersService: TiersService;
+afterEach(() => jest.restoreAllMocks());
 
 describe('Testing the handler when an invoice is completed', () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-    stripe = new Stripe('mock-key', { apiVersion: '2024-04-10' }) as jest.Mocked<Stripe>;
-    usersRepository = testFactory.getUsersRepositoryForTest();
-    displayBillingRepository = {} as DisplayBillingRepository;
-    couponsRepository = testFactory.getCouponsRepositoryForTest();
-    usersCouponsRepository = testFactory.getUsersCouponsRepositoryForTest();
-    tierRepository = testFactory.getTiersRepository();
-    usersTiersRepository = testFactory.getUsersTiersRepository();
-    productsRepository = testFactory.getProductsRepositoryForTest();
-
-    cacheService = new CacheService(config);
-    storageService = new StorageService(config, axios);
-    bit2MeService = new Bit2MeService(config, axios);
-    paymentService = new PaymentService(stripe, productsRepository, bit2MeService);
-
-    usersService = new UsersService(
-      usersRepository,
-      paymentService,
-      displayBillingRepository,
-      couponsRepository,
-      usersCouponsRepository,
-      config,
-      axios,
-    );
-
-    tiersService = new TiersService(
-      usersService,
-      paymentService,
-      tierRepository,
-      usersTiersRepository,
-      storageService,
-      config,
-    );
-
-    objectStorageService = new ObjectStorageService(paymentService, config, axios);
-    objectStorageWebhookHandler = new ObjectStorageWebhookHandler(objectStorageService, paymentService);
-    determineLifetimeConditions = new DetermineLifetimeConditions(paymentService, tiersService);
-    invoiceCompletedHandler = new InvoiceCompletedHandler({
-      logger: getLogger(),
-      determineLifetimeConditions,
-      objectStorageWebhookHandler,
-      paymentService,
-      storageService,
-      tiersService,
-      usersService,
-      cacheService,
-    });
-  });
-
-  afterEach(() => jest.restoreAllMocks());
-
   describe('Run the process', () => {
     test('When the invoice is not paid, then a log indicating so is printed and it should not continues', async () => {
       const mockedCustomer = getCustomer();
