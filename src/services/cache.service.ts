9ec1d314856d638ed018cdb6fd381c832694dab1
Sergio Gutiérrez <34506328+sg-gs@users.noreply.github.com>
2026-02-13 21:23:22 +0100

    Simplify Redis initialization in CacheService
    Refactor Redis initialization to simplify configuration.

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -9,10 +9,7 @@ const FOUR_HOURS_EXPIRATION_IN_SECONDS = 4 * 60 * 60;
 export default class CacheService {
   private readonly redis: Redis;
   constructor(config: AppConfig) {
-    this.redis =
-      config.NODE_ENV === 'production'
-        ? new Redis({ host: config.REDIS_HOST, password: config.REDIS_PASSWORD })
-        : new Redis({ host: config.REDIS_HOST });
+    this.redis = new Redis({ host: config.REDIS_HOST });
   }
 
   private buildSubscriptionKey(customerId: string, userType: UserType = UserType.Individual): string {
