f07dcc3a2e53429c6e35c73f951688892e07b0a3
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-02-18 09:50:53 +0100

    feat(health): add rate limit

--- src/controller/health.controller.ts
+++ src/controller/health.controller.ts
@@ -3,8 +3,19 @@ import { HealthService } from '../services/health.service';
 
 export default function healthController(healthService: HealthService) {
   return async function (fastify: FastifyInstance) {
-    fastify.get('/health', async () => {
-      return await healthService.checkHealth();
-    });
+    fastify.get(
+      '/health',
+      {
+        config: {
+          rateLimit: {
+            max: 80,
+            timeWindow: '1 minute',
+          },
+        },
+      },
+      async () => {
+        return await healthService.checkHealth();
+      },
+    );
   };
 }
