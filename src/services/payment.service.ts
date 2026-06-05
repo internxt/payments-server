6e35252aab0451f59f258b898ccf38817a99e19f
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-06-05 09:37:30 +0200

    feat(invoices): get paid invoices from user

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -704,12 +704,14 @@ export class PaymentService {
     customerId: CustomerId,
     pagination: { limit?: number; startingAfter?: string },
     subscriptionId?: SubscriptionId,
+    status?: Stripe.Invoice['status'],
   ): Promise<Invoice[]> {
     const res = await this.provider.invoices.list({
       customer: customerId,
       limit: pagination.limit,
       starting_after: pagination.startingAfter,
       subscription: subscriptionId,
+      ...(status && { status }),
     });
 
     return res.data;
@@ -729,7 +731,7 @@ export class PaymentService {
   ) {
     const { limit, startingAfter } = pagination;
 
-    const invoices = await this.getInvoicesFromUser(customerId, { limit, startingAfter }, subscriptionId);
+    const invoices = await this.getInvoicesFromUser(customerId, { limit, startingAfter }, subscriptionId, 'paid');
 
     const invoicesMapped = invoices
       .filter((invoice) =>
