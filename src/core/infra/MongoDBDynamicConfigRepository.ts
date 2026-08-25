b1b149b14ed7ba9b87303842265859f97cbec063
Xavier Abad <77491413+xabg2@users.noreply.github.com>
2026-08-25 10:05:40 +0200

    feat: add log

--- src/core/infra/MongoDBDynamicConfigRepository.ts
+++ src/core/infra/MongoDBDynamicConfigRepository.ts
@@ -1,6 +1,7 @@
 import { Collection, MongoClient } from 'mongodb';
 
 import { DynamicConfigEntry, DynamicConfigKey, DynamicConfigRepository } from './DynamicConfigRepository';
+import Logger from '../../Logger';
 
 export class MongoDBDynamicConfigRepository implements DynamicConfigRepository {
   private readonly collection: Collection<DynamicConfigEntry>;
@@ -11,6 +12,7 @@ export class MongoDBDynamicConfigRepository implements DynamicConfigRepository {
 
   async get(key: DynamicConfigKey): Promise<string | null> {
     const entry = await this.collection.findOne({ key });
+    if (!!entry?.value) Logger.info(`[DYNAMIC CONFIG]: Value got from DB for ${key}`);
 
     return entry?.value ?? null;
   }
